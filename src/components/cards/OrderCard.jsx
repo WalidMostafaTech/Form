a19de4378d0529754e924fdf4d9ef61fb17018e8
Walid Mostafa <amadalioo980@gmail.com>
2026-04-15 14:06:44 +0200

    change oeder details

--- src/components/cards/OrderCard.jsx
+++ src/components/cards/OrderCard.jsx
@@ -1,12 +1,19 @@
 import UEAIcon from "@/components/common/UEAIcon";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { HiCalendarDateRange } from "react-icons/hi2";
 import { Button } from "../ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 const OrderCard = ({ item }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
   const formatDate = (dateString) => {
     return new Date(dateString).toLocaleDateString("en-US", {
       day: "numeric",
@@ -32,56 +39,21 @@ const OrderCard = ({ item }) => {
         <h3 className="text-lg font-semibold line-clamp-2">{item.name}</h3>
         <p>{item.weight}</p>
 
-        {!showDetails && (
-          <>
-            <p className="py-1 px-2 w-fit text-xs text-primary bg-primary-foreground rounded-md">
-              {item.status}
-            </p>
+        <p className="py-1 px-2 w-fit text-xs text-primary bg-primary-foreground rounded-md">
+          {item.status}
+        </p>
 
-            <div className="flex items-center gap-2">
-              <span className="p-1 text-lg text-primary bg-primary-foreground rounded-md">
-                <HiCalendarDateRange />
-              </span>
-              <p>{formatDate(item.created_at)}</p>
-            </div>
-          </>
-        )}
-
-        {showDetails && (
-          <>
-            <div className="flex items-center justify-between flex-wrap gap-2 font-semibold">
-              <p>
-                {item?.order_items[0]?.weight}{" "}
-                {item?.order_items[0]?.weight_unit}
-              </p>
-
-              <p className="text-primary text-lg flex items-center gap-1">
-                {item?.order_items[0]?.price} <UEAIcon />
-              </p>
-            </div>
-
-            <div
-              className="text-muted-foreground rich_content"
-              dangerouslySetInnerHTML={{
-                __html: item?.order_items[0]?.description,
-              }}
-            />
-          </>
-        )}
+        <div className="flex items-center gap-2">
+          <span className="p-1 text-lg text-primary bg-primary-foreground rounded-md">
+            <HiCalendarDateRange />
+          </span>
+          <p>{formatDate(item.created_at)}</p>
+        </div>
 
         <div className="flex items-center flex-wrap justify-between gap-2">
           <p className="text-lg  text-primary flex items-center gap-1">
             {item.final_total} <UEAIcon />
           </p>
-
-          {showDetails && (
-            <p
-              className="text-primary bg-primary-foreground py-1 px-2 rounded-md 
-            min-w-14 text-center text-lg font-semibold border"
-            >
-              {item.order_items_count}
-            </p>
-          )}
         </div>
 
         <div className="flex items-center flex-wrap justify-end gap-2">
@@ -94,13 +66,78 @@ const OrderCard = ({ item }) => {
             {t("DownloadInvoice")}
           </Button>
 
-          <Button
-            size="xs"
-            variant={showDetails ? "destructive" : "outline"}
-            onClick={() => setShowDetails(!showDetails)}
-          >
-            {showDetails ? t("HideDetails") : t("ViewDetails")}
-          </Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button size="xs" variant={"outline"}>
+                {t("ViewDetails")}
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="lg:max-w-xl">
+              <DialogHeader>
+                <DialogTitle>
+                  {t("OrderDetails")}{" "}
+                  <span className="text-primary text-sm">
+                    ( {t("OrderCount")} {item.order_items_count})
+                  </span>
+                </DialogTitle>
+                <DialogDescription></DialogDescription>
+              </DialogHeader>
+
+              <div className="space-y-2 custom_scrollbar max-h-[60vh] overflow-y-auto">
+                {item.order_items.map((item) => (
+                  <div
+                    key={item.id}
+                    className="p-3 border rounded-lg items-start flex gap-4"
+                  >
+                    <div className="w-28 aspect-square overflow-hidden rounded-lg">
+                      <img
+                        loading="lazy"
+                        src={item.image}
+                        alt={item.name}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+
+                    <div className="flex-1 font-semibold flex flex-col justify-between gap-2 h-full">
+                      <div className="flex items-start justify-between gap-2">
+                        <h3 className="text-lg  line-clamp-2">{item.name}</h3>
+                      </div>
+
+                      <p>
+                        {item.weight} {item.weight_unit}
+                      </p>
+
+                      <p className=" text-primary text-lg flex items-center gap-1">
+                        {item.price} <UEAIcon />
+                      </p>
+
+                      <div
+                        className="text-gray-600 text-sm"
+                        dangerouslySetInnerHTML={{ __html: item.description }}
+                      />
+
+                      <div className="flex items-center flex-wrap justify-between gap-2">
+                        <span className="p-1 min-w-16 text-lg text-center font-semibold text-primary bg-primary-foreground rounded-md border">
+                          {item.quantity}
+                        </span>
+
+                        <p className="text-3xl font-semibold text-primary flex items-center gap-1">
+                          {item.price * item.quantity}{" "}
+                          <UEAIcon className="w-7 h-7" />
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button>{t("close")}</Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
     </div>
